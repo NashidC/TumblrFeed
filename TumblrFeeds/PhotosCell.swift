5df737f4f28421bdb4b9060a975312a920a4c826
NashidC <nash.chowdhury@gmail.com>
2017-02-02 16:28:00 -0500

    slight update

---
+++ TumblrFeeds/PhotosCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosTableViewCell.swift
+//  TumblrFeeds
+//
+//  Created by Nashid  on 2/2/17.
+//  Copyright © 2017 Nashid Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TumblrFeeds/PhotosViewController.swift
@@ -0,0 +1,87 @@
+//
+//  PhotosViewController.swift
+//  TumblrFeeds
+//
+//  Created by Nashid  on 2/2/17.
+//  Copyright © 2017 Nashid Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+ 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+       var posts: [NSDictionary]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        
+
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                          self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                    }
+                }
+        });
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let posts = posts {
+            return posts.count
+        } else {
+            return 0
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotosCell
+        
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
